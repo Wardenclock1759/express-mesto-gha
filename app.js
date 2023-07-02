e479d9a1dfdb3fd6ac57d024d5ef20a888f4b674
Misha Kukarkin <wardenclock@gmail.com>
2023-07-02 22:13:48 +0500

    removed body-parcer from req

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 
 const routes = require('./routes/index');
 const { NOT_FOUND_CODE, NOT_FOUND_MESSAGE } = require('./constants');
 
 const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
