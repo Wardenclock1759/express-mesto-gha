0948f90bc002c6d2865586a5b9fdb8eac943cdf4
Misha Kukarkin <wardenclock@gmail.com>
2023-07-02 15:45:24 +0500

    fixed exports

--- controllers/constants.js
+++ controllers/constants.js
@@ -1,5 +1,5 @@
-export const BAD_REQUEST_CODE = 400;
-export const NOT_FOUND_CODE = 404;
-export const INTERNAL_CODE = 500;
+module.exports.BAD_REQUEST_CODE = 400;
+module.exports.NOT_FOUND_CODE = 404;
+module.exports.INTERNAL_CODE = 500;
 
-export const INTERNAL_MESSAGE = 'Ошибка сервера';
+module.exports.INTERNAL_MESSAGE = 'Ошибка сервера';
