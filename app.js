e2583abdc35de771d368b3fadbfba55ca23e907b
Misha Kukarkin <wardenclock@gmail.com>
2023-07-02 17:05:04 +0500

    series of fixes for autotests #1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-const { NOT_FOUND_CODE, NOT_FOUND_MESSAGE } = require('constants');
+const { NOT_FOUND_CODE, NOT_FOUND_MESSAGE } = require('./constants');
 
 const app = express();
 app.use(bodyParser.json());
