550b37664db96474529f80dbe0ee9b6544aebcc9
Misha Kukarkin <wardenclock@gmail.com>
2023-07-10 19:44:48 +0500

    changed from optional to required

--- routes/users.js
+++ routes/users.js
@@ -14,15 +14,15 @@ router.get('/:userId', celebrate({
 
 router.patch('/me', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30).optional(),
-    about: Joi.string().min(2).max(30).optional(),
+    name: Joi.string().min(2).max(30).required(),
+    about: Joi.string().min(2).max(30).required(),
   }),
 }), updateUserInfo);
 router.patch('/me/avatar', celebrate({
   body: Joi.object().keys({
     avatar: Joi.string().uri({
       scheme: ['http', 'https'],
-    }).optional(),
+    }).required(),
   }),
 }), updateUserAvatar);
 
