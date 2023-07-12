68b1a7435070aff179eee68787ea6e1e46dbf5f1
Misha Kukarkin <wardenclock@gmail.com>
2023-07-12 08:04:17 +0500

    removed unused constants

--- constants.js
+++ constants.js
@@ -1,10 +1,7 @@
 module.exports.STATUS_CREATED = 201;
-module.exports.BAD_REQUEST_CODE = 400;
-module.exports.INTERNAL_CODE = 500;
 
 module.exports.AUTHENTICATED = 'Авторизация прошла успешно';
 module.exports.FORBITTEN_MESSAGE = 'У вас нет прав для модификации этой карточки';
 module.exports.NOT_FOUND_MESSAGE = 'По вашему запросу ничего не найдено';
-module.exports.INTERNAL_MESSAGE = 'Ошибка сервера';
 
 module.exports.urlRegex = /https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,}/;
